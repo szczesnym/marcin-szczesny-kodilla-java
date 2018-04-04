9b6d67ddd78e5e5e130dbaa2ce101af9650d9608
Marcin Szczesny <marcins@oerlemans-foods.nl>
2018-04-04 16:23:19 +0200

    Firs Commit

---
+++ src/main/java/com/kodilla/KodilaCourseApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodilaCourseApplication.class, args);
+    }
+}
